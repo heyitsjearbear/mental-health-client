bd34ac8f9e727eb4981917b83584e14d781bf85a
heyitsjearbear <jereberries1215@gmail.com>
2023-12-29 04:28:15 -0800

    Added logout button to home screen

--- src/home/components/Home.tsx
+++ src/home/components/Home.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../authentication/context/authContext";
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
-}
-
-export default Home;
+    const navigate = useNavigate();
+    const { logout } = useContext(AuthContext);
+  
+    const handleLogout = () => {
+      logout();
+      navigate('/auth'); // Update this to your actual authentication route
+    };
+  
+    return (
+      <div>
+        Home
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    );
+  }
+  
+  export default Home;
