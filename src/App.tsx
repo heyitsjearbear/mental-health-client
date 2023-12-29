68458db412dbd8a7e6d6b176f2ecd01057911efc
heyitsjearbear <jereberries1215@gmail.com>
2023-12-29 02:28:57 -0800

    Added ability to switch between register and sign in pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css';
-import Login from './authentication/components/Login';
+import "./App.css";
+import Welcome from "./authentication/components/Welcome";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
 function App() {
   return (
-    <>
-    <Login />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/auth" element={<Welcome />} />
+        <Route path="/" element={<Navigate to="/auth" />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/authentication/components/Welcome.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Container, Form, Button, Card } from "react-bootstrap";
+import "./styles.css";
+const Welcome: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (isLogin) {
+      // Handle login logic here
+      console.log("Login:", email, password);
+    } else {
+      // Handle registration logic here
+      console.log("Register:", email, password);
+    }
+  };
+
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Card className="panel" style={{ width: "400px" }}>
+        <Card.Body>
+          <Card.Title className="text-center mb-4">
+            {isLogin ? "Welcome" : "Register"}
+          </Card.Title>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="w-100">
+              {isLogin ? "Login" : "Register"}
+            </Button>
+            <Button
+              variant="secondary"
+              className="w-100 mt-3"
+              onClick={() => setIsLogin(!isLogin)} // Switch between login and register
+            >
+              {isLogin ? "Not a user?" : "Already a user?"}
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default Welcome;
